5c6c427b8d251fa56b7707bff9a2a211ebe5d853
Agoth Arop <agoth.a3@gmail.com>
2025-08-10 21:47:17 -0600

    Added search functionality and location handling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-import './style.css';
+import "./style.css";
 
+const form = document.querySelector("form");
+const searchInput = document.querySelector("#search");
+const weatherSection = document.querySelector(".weather-section");
+
+form.addEventListener("submit", (e) => {
+  const location = searchInput.value;
+  e.preventDefault();
+  if (location) {
+    console.log(`Searching for weather in ${location}`);
+    getWeatherInfo(location);
+  }
+});
+
+async function getWeatherInfo(location) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=9MHVYPDZHDF6Q88W7JB5KUQVL&contentType=json`
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+function parseWeatherData(data) {
+  return {};
+}
